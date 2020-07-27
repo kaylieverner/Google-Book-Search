22c267c1048e23011ec025ec24308d9aade30e5c
Kaylie V <k.verner37@gmail.com>
2020-07-26 22:23:25 -0400

    updating searchbox component button to iconbutton instead of button

--- client/src/components/SearchBox/index.js
+++ client/src/components/SearchBox/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Button, TextField } from '@material-ui/core';
+import { TextField } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
 
 const useStyles = makeStyles((theme) => ({
     button: {
@@ -18,13 +19,9 @@ export default function SearchBox(props) {
         <h3>{props.title}</h3>
             <form className="form" noValidate autoComplete="off">
                 <TextField id="outlined-basic" label={props.label}/>
-                <Button
-                className={classes.button}
-                variant="contained"
-                color="primary"
-                endIcon={<SearchIcon/>}>
-                {props.btnText}
-                </Button>
+                <IconButton color="primary" aria-label="upload picture" component="span">
+                    <SearchIcon />
+                </IconButton>
             </form>
         </div>
     </div>
