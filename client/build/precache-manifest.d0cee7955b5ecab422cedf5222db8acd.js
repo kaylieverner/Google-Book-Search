86e1c14624b77b8b2008b24789d91620d961ca1e
Kaylie V <k.verner37@gmail.com>
2020-07-31 22:29:02 -0400

    build folder

---
+++ client/build/precache-manifest.d0cee7955b5ecab422cedf5222db8acd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2194ebdff569eb449aea",
+    "url": "/static/css/main.6ea8a776.chunk.css"
+  },
+  {
+    "revision": "2194ebdff569eb449aea",
+    "url": "/static/js/main.c67e1cd8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b19effd8b400f3ee9c59",
+    "url": "/static/js/2.9e8bfb38.chunk.js"
+  },
+  {
+    "revision": "fade787a0b2d53d0c8b9ae2db39f5a68",
+    "url": "/index.html"
+  }
+];
